af369003d7a530225fc081d69fad34bac4d0663a
Mohammad hadi <m6336.programer@gmail.com>
2024-06-11 05:00:26 +0330

    modify

---
+++ testSMS.js
@@ -0,0 +1,8 @@
+// import SendAlertSMSIR from './smsir_mh_js6.js';
+
+// const apiKey = '121212121212121212121';  //Example API Key 
+// const to = '09100059323';  //Example Phone Number
+// const message = 'Hello,this test message!'; //Max 24 Character Length
+// const templateId = '750016';  //Example template ID 
+
+// SendAlertSMSIR(to, message, apiKey, templateId);
